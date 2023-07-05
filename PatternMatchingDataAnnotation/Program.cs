a72969929dc4aee3d627ec6bb7876b903adbc67c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-06 00:38:36 +0530

    [Ritvik]Add: Welcome Message

---
+++ PatternMatchingDataAnnotation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PatternMatchingDataAnnotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Annotation");
+        }
+    }
+}
