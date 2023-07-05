1dd9eb12c0d31c2d9c8e6e43aeb7ea952a27399f
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-06 00:57:15 +0530

    [Ritvik]Add: Validating FirstName

---
+++ PatternMatchingDataAnnotation/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatchingDataAnnotation
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Employee{0} is required")]
+        [StringLength(50,MinimumLength = 3,ErrorMessage = "First name starts with Cap and has minimum 3 characters")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+    }
+}
+ 
---
+++ PatternMatchingDataAnnotation/Validate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatchingDataAnnotation
+{
+    public class Validate
+    {
+        public static void Validation()
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Ritvik";
+
+            ValidationContext validation = new ValidationContext(employee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            bool valid = Validator.TryValidateObject(employee, validation, results, true);
+            if (valid)
+            {
+                Console.WriteLine("FirstName: "+employee.FirstName); Console.WriteLine("Is Valid");
+            }
+            else
+            {
+                foreach (ValidationResult result in results)
+                {
+                    Console.WriteLine(result.ErrorMessage);
+                }
+            }
+        }
+    }
+}
